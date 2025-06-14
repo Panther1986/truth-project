18d4de9593578159f8febe9c70b2e72d4830d6c3
Olha Meshcheriakova <polzovatel77@gmail.com>
2025-06-14 19:46:56 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,25 @@
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
+import dotenv from 'dotenv';
 dotenv.config();
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    base: '/',
-    build: {
-      sourcemap: true,
-      outDir: 'dist',
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
+export default defineConfig(({ command }) => ({
+  root: 'src',
+  base: './',
+  build: {
+    outDir: '../dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) return 'vendor';
         },
+        entryFileNames: 'commonHelpers.js',
       },
     },
-    plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
-  };
-});
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+}));
