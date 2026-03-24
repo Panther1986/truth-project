b7c36f276abc0b5afdac00264dd96483c3d2fe81
Olha Meshcheriakova <polzovatel77@gmail.com>
2026-03-24 10:58:50 +0200

    add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/header.js';
 import './js/thoughts-and-reforms.js';
-import './js/low-and-narrative.js';
+// import './js/low-and-narrative.js';
 import './js/responsible-investment.js';
 import './js/sens-vs-chaos.js';
 import './js/localization.js';
